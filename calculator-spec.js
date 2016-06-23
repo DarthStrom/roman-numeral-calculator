9656e09332ba65394a3992adbd0d52c0df9c2f89
Jason Duffy <jasonsduffy@gmail.com>
2016-06-23 13:03:13 -0400

    some extra tests for blank strings

--- calculator-spec.js
+++ calculator-spec.js
@@ -2,6 +2,12 @@ var calculator = require('./calculator');
 
 describe('calculator', function () {
 
+  it('treats empty strings as 0', function () {
+    expect(calculator.add("", "")).toBe("");
+    expect(calculator.add("I", "")).toBe("I");
+    expect(calculator.add("", "II")).toBe("II");
+  });
+
   it('adds 1s', function () {
     expect(calculator.add("I", "I")).toBe("II");
   });
