7871d5d72a47eaa85e3052fddd3c57be14d32f88
Jason Duffy <jasonsduffy@gmail.com>
2016-06-23 15:38:25 -0400

    the rest of the glyphs

--- converter-spec.js
+++ converter-spec.js
@@ -10,6 +10,32 @@ describe('converter', function () {
 
     it('can convert II', function () {
       expect(converter.romanToNumber("II")).toBe(2);
-    })
+    });
+
+    it('can convert V', function () {
+      expect(converter.romanToNumber("V")).toBe(5);
+    });
+
+    it('can convert X', function () {
+      expect(converter.romanToNumber("X")).toBe(10);
+    });
+
+    it('can convert L', function () {
+      expect(converter.romanToNumber("L")).toBe(50);
+    });
+
+    it('can convert C', function () {
+      expect(converter.romanToNumber("C")).toBe(100);
+    });
+
+    it('can convert D', function () {
+      expect(converter.romanToNumber("D")).toBe(500);
+    });
+
+    it('can convert M', function () {
+      expect(converter.romanToNumber("M")).toBe(1000);
+    });
   });
 });
+
+// XLCDM
--- converter.js
+++ converter.js
@@ -1,3 +1,18 @@
+var glyphs = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
+
 exports.romanToNumber = function (roman) {
-  return roman.length;
+  var result = 0;
+  for (var i = 0; i < roman.length; i++) {
+    result += glyphs[roman[i]];
+  }
+
+  return result;
 };
