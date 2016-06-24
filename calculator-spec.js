777dc50bfb6dac37bc76538c2a92a487ec678bb9
Jason Duffy <jasonsduffy@gmail.com>
2016-06-23 23:11:10 -0400

    subtraction

--- calculator-spec.js
+++ calculator-spec.js
@@ -2,34 +2,52 @@ var calculator = require('./calculator');
 
 describe('calculator', function () {
 
-  it('treats empty strings as 0', function () {
-    expect(calculator.add("", "")).toBe("");
-    expect(calculator.add("I", "")).toBe("I");
-    expect(calculator.add("", "II")).toBe("II");
+  describe('addition', function () {
+
+    it('treats empty strings as 0', function () {
+      expect(calculator.add("", "")).toBe("");
+      expect(calculator.add("I", "")).toBe("I");
+      expect(calculator.add("", "II")).toBe("II");
+    });
+
+    it('adds 1s', function () {
+      expect(calculator.add("I", "I")).toBe("II");
+    });
+
+    it('adds 1 and 2', function () {
+      expect(calculator.add("I", "II")).toBe("III");
+    });
+
+    it('adds 2 and 2', function () {
+      expect(calculator.add("II", "II")).toBe("IV");
+    });
+
+    it('can add larger numbers by concatenation', function () {
+      expect(calculator.add("XX", "II")).toBe("XXII");
+    });
+
+    it('concatenates in the right order', function () {
+      expect(calculator.add("XIV", "LX")).toBe("LXXIV");
+    });
+
+    it('upgrades the glyph instead of repeating', function () {
+      expect(calculator.add("D", "D")).toBe("M");
+    });
   });
 
-  it('adds 1s', function () {
-    expect(calculator.add("I", "I")).toBe("II");
+  describe('subtraction', function () {
+
+    it('subtracts', function () {
+      expect(calculator.subtract("", "")).toBe("");
+      expect(calculator.subtract("I", "")).toBe("I");
+      expect(calculator.subtract("II", "")).toBe("II");
+      expect(calculator.subtract("I", "I")).toBe("");
+      expect(calculator.subtract("II", "I")).toBe("I");
+      expect(calculator.subtract("II", "II")).toBe("");
+      expect(calculator.subtract("XX", "II")).toBe("XVIII");
+      expect(calculator.subtract("LX", "XIV")).toBe("XLVI");
+      expect(calculator.subtract("M", "D")).toBe("D");
+    })
   });
-
-  it('adds 1 and 2', function () {
-    expect(calculator.add("I", "II")).toBe("III");
-  });
-
-  it('adds 2 and 2', function () {
-    expect(calculator.add("II", "II")).toBe("IV");
-  });
-
-  it('can add larger numbers by concatenation', function () {
-    expect(calculator.add("XX", "II")).toBe("XXII");
-  });
-
-  it('concatenates in the right order', function () {
-    expect(calculator.add("XIV", "LX")).toBe("LXXIV");
-  })
-
-  it('upgrades the glyph instead of repeating', function () {
-    expect(calculator.add("D", "D")).toBe("M");
-  })
 
 });
--- calculator.js
+++ calculator.js
@@ -8,5 +8,13 @@ exports.add = function(x, y) {
   return converter.numberToRoman(sum);
 };
 
+exports.subtract = function(x, y) {
+  var xNumber = converter.romanToNumber(x);
+  var yNumber = converter.romanToNumber(y);
+  var difference = xNumber - yNumber;
+
+  return converter.numberToRoman(difference);
+}
+
 // note: maybe create arabic and roman numeral classes that can convert
 // themselves instead of having a converter
